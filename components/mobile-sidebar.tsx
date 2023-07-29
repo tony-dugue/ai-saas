6b8fb7b1fcab65af407ed361738dcf7a508f7476
Tony Dugué <duguetony@gmail.com>
2023-07-29 12:45:55 +0200

    add sidebar with mobile version & prepare navbar

---
+++ components/mobile-sidebar.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Menu } from "lucide-react";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import Sidebar from '@/components/sidebar';
+
+const MobileSidebar = () => {
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <Button variant="ghost" size="icon" className="md:hidden">
+          <Menu />
+        </Button>
+      </SheetTrigger>
+
+      <SheetContent side="left" className="p-0">
+        <Sidebar />
+      </SheetContent>
+    </Sheet>
+  )
+}
+
+export default MobileSidebar;
+
+
+
